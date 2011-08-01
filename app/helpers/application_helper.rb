cbeacfddf98c7e769e177808fcb7ee1082b52814
Piotr Milewski <milewski.piotr@gmail.com>
2011-08-02 01:36:51 +0200

    take pages#home as default page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def title
+		base_title = "Ruby on Rails 3: Twitt3r-like application"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
 end
