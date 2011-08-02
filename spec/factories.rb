221b74e615b4ef8ff859733ad6d1168c808de1df
Piotr Milewski <milewski.piotr@gmail.com>
2011-08-02 16:05:26 +0200

    User model with password, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+	user.name 									"Piotr Milewski"
+	user.email									"pm@twitt3r.heroku.com"
+	user.password								"foobar"
+	user.password_confirmation	"foobar"
+end
